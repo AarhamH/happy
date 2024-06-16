73e68aa5ff96730d3b77617cc14d36d1f82bc49f
AarhamH <ahaider@cayenta.com>
2024-06-16 00:04:04 -0700

    Added expression definition and running implementation

--- ParserCombinator.hs
+++ ParserCombinator.hs
@@ -106,3 +106,51 @@ parens m = do
   n <- m
   reserved ")"
   return n
+
+data Expr
+  = Add Expr Expr
+  | Mul Expr Expr
+  | Sub Expr Expr
+  | Lit Int
+  deriving Show
+
+eval :: Expr -> Int
+eval ex = case ex of
+  Add a b -> eval a + eval b
+  Mul a b -> eval a * eval b
+  Sub a b -> eval a - eval b
+  Lit n   -> n
+
+int :: Parser Expr
+int = do
+  n <- number
+  return (Lit n)
+
+expr :: Parser Expr
+expr = term `chainl'` addop
+
+term :: Parser Expr
+term = factor `chainl'` mulop
+
+factor :: Parser Expr
+factor =
+      int
+  <|> parens expr
+
+infixOp :: String -> (a -> a -> a) -> Parser (a -> a -> a)
+infixOp x f = reserved x >> return f
+
+addop :: Parser (Expr -> Expr -> Expr)
+addop = (infixOp "+" Add) <|> (infixOp "-" Sub)
+
+mulop :: Parser (Expr -> Expr -> Expr)
+mulop = infixOp "*" Mul
+
+run :: String -> Expr
+run = runParser expr
+
+main :: IO ()
+main = forever $ do
+  putStr "> "
+  a <- getLine
+  print $ eval $ run a

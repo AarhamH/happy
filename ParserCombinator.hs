b1df393b53ab626a9b0debcea71b87eb986d5246
AarhamH <ahaider@cayenta.com>
2024-06-15 18:35:39 -0700

    Added instance of monadic parser

--- ParserCombinator.hs
+++ ParserCombinator.hs
@@ -12,3 +12,25 @@ runParser m s = case parse m s of
     [(m1,[])]    -> res   
     [(_,s1)]     -> error "Parser did not consume stream"
     _            -> error "Parser Error"
+
+item :: Parser Char
+item = Parser $ \s -> case s of
+    []       -> []
+    (c:cs)   -> [(c,cs)]
+
+bind :: Parser a -> (a -> Parser b) -> Parser b
+bind p pf = Parser $ \s -> concatMap (\(a,s') -> parse (pf a) s') $ parse p s
+
+unit :: a -> Parser a
+unit a = Parser (\s -> [(a,s)])
+
+instance Functor Parser where
+    fmap f (Parser cs) = Parser (\s -> [(f a,b | (a,b) <- cs s)])
+
+instance Applicative Parser where
+    pure = return
+    (Parser c1) <*> (Parser c2) = Parser (\s -> [(fa, s2) | (f,s1) <- cs1,s, (a,s2 <- cs2 s1)])
+
+instance Monad Parser where
+    return = unit
+    (>>=) = bind

1035615a855994e86113d7911fb2b01dbf995687
AarhamH <ahaider@cayenta.com>
2024-06-30 12:24:52 -0700

    Removed whitespace sensitivity

--- app/Parser.hs
+++ app/Parser.hs
@@ -1,12 +1,15 @@
+{-# OPTIONS_GHC -Wno-unused-matches #-}
 module Parser where
 
 import Text.ParserCombinators.Parsec hiding (spaces)
-import System.Environment
 
 symbol :: Parser Char 
 symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
 
+spaces :: Parser () 
+spaces = skipMany1 space
+
 readExpr :: String -> String
-readExpr input = case parse symbol "lisp" input of
+readExpr input = case parse (spaces >> symbol) "lisp" input of
     Left err -> "No match: " ++ show err
     Right val -> "Found value"

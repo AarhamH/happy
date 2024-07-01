746a8b3eebf80e0ae1488d48abd73effbf50b4b5
AarhamH <ahaider@cayenta.com>
2024-06-30 23:40:14 -0700

    Trim type name when displaying values

--- app/types/Values.hs
+++ app/types/Values.hs
@@ -6,4 +6,17 @@ data Values = Atom String
             | Number Integer
             | String String
             | Bool Bool
-            deriving (Show)
+
+unwordsList :: [Values] -> String
+unwordsList = unwords . map showValue 
+
+showValue :: Values -> String 
+showValue (Atom name) = name
+showValue (String contents) = "\"" ++ contents ++ "\""
+showValue (Number contents) = show contents
+showValue (List contents) = "(" ++ unwordsList contents ++ ")"
+showValue (ImproperList head tail) = "(" ++ unwordsList head ++ " . " ++ showValue tail ++ ")"
+showValue (Bool True) = "#t"
+showValue (Bool False) = "#f"
+
+instance Show Values where show = showValue

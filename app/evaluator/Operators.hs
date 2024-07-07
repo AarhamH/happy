5c0b087758f6b1e0c91905a61d1176ea37a19f7a
AarhamH <ahaider@cayenta.com>
2024-07-07 12:33:19 -0700

    Added equality operator

--- app/evaluator/Operators.hs
+++ app/evaluator/Operators.hs
@@ -2,6 +2,7 @@ module Operators where
 
 import Values
 import Control.Monad.Except
+import qualified Data.Functor
 
 operators :: [(String, [Values] -> ThrowsError Values)]
 operators = [ ("+", numberOp (+))
@@ -10,19 +11,29 @@ operators = [ ("+", numberOp (+))
             , ("/", numberOp div)
             , ("mod", numberOp mod)
             , ("quotient", numberOp quot)
-            , ("remainder", numberOp rem) 
-            ]
+            , ("remainder", numberOp rem)
+            , ("=", boolOp (==))
+  ]
 
 numberOp :: (Integer -> Integer -> Integer) -> [Values] -> ThrowsError Values
-numberOp op [] = throwError $ ArgumentNumber 2 []
-numberOp op singleVal@[_] = throwError $ ArgumentNumber 2 singleVal
-numberOp op params = mapM numberUnpacker params >>= return . Number . foldl1 op
+numberOp _ [] = throwError $ ArgumentNumber 2 []
+numberOp _ singleVal@[_] = throwError $ ArgumentNumber 2 singleVal
+numberOp op params = mapM numberUnpacker params Data.Functor.<&> (Number . foldl1 op)
 
 numberUnpacker :: Values -> ThrowsError Integer
 numberUnpacker (Number n) = return n
-numberUnpacker (String s) = let parsed = reads s in if null parsed 
+numberUnpacker (String s) = let parsed = reads s in if null parsed
                                                     then throwError $ TypeMismatch "number" $ String s
-                                                    else return $ fst $ parsed !! 0
+                                                    else return $ fst $ head parsed
 numberUnpacker (List [n]) = numberUnpacker n
 numberUnpacker notNum = throwError $ TypeMismatch "number" notNum
 
+boolOpBase :: (Values -> ThrowsError a) -> (a -> a -> Bool) -> [Values] -> ThrowsError Values
+boolOpBase unpackerf op args = if length args /= 2
+                           then throwError $ ArgumentNumber 2 args
+                           else do left <- unpackerf $ head args
+                                   right <- unpackerf $ args !! 1
+                                   return $ Bool $ left `op` right
+
+boolOp :: (Integer -> Integer -> Bool) -> [Values] -> ThrowsError Values
+boolOp = boolOpBase numberUnpacker

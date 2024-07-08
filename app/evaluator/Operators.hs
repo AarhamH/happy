097960006fb570d35b2529618b4f4a7a69874418
AarhamH <ahaider@cayenta.com>
2024-07-07 19:40:23 -0700

    Unpacked list operations into Operations file

--- app/evaluator/Operators.hs
+++ app/evaluator/Operators.hs
@@ -2,7 +2,6 @@ module Operators where
 
 import Values
 import Control.Monad.Except
-import ListConstructors
 import qualified Data.Functor
 
 operators :: [(String, [Values] -> ThrowsError Values)]
@@ -70,3 +69,23 @@ boolOp = boolOpBase boolUnpacker
 
 stringOp :: (String -> String -> Bool) -> [Values] -> ThrowsError Values
 stringOp = boolOpBase stringUnpacker
+
+listHead :: [Values] -> ThrowsError Values
+listHead [List(x:_)] = return x
+listHead [ImproperList(x:_) _] = return x
+listHead [badType] = throwError $ TypeMismatch "pair" badType
+listHead badArgs = throwError $ ArgumentNumber 1 badArgs
+
+listTail :: [Values] -> ThrowsError Values
+listTail [List(_:xs)] = return $ List xs
+listTail [ImproperList[_] xs] = return xs
+listTail [ImproperList(_:xs) xs1] = return $ ImproperList xs xs1
+listTail [badType] = throwError $ TypeMismatch "pair" badType
+listTail badArgs = throwError $ ArgumentNumber 1 badArgs
+
+listConstruct :: [Values] -> ThrowsError Values
+listConstruct [x, List []] = return $ List[x]
+listConstruct [x, List xs] = return $ List $ (x:xs)
+listConstruct [x, ImproperList xs xs1] = return $ ImproperList (x:xs) xs1
+listConstruct [x, y] = return $ ImproperList [x] y
+listConstruct badList = throwError $ ArgumentNumber 2 badList
